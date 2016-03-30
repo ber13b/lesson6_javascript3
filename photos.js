37100321f9393c3a31230063abaa7549c63e2e12
ber13b <ber13b@my.fsu.edu>
2016-03-30 22:43:38 +0000

    lesson 6 final commit

--- photos.js
+++ photos.js
@@ -1,7 +1,7 @@
 /*    
     Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author:Brandon Rhea 
+    Date:   3/23/2016
     Filename: photos.js
  */
 
@@ -10,7 +10,17 @@
 /* global variables */
 var photoOrder = [1,2,3,4,5];
 var figureCount = 3;
-
+function populateFigures() {
+   var filename;
+   var currentFig;
+   
+   for (var i = 1;i < 4; i++) {
+      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+      currentFig = document.getElementsByTagName("img")[i - 1];
+      currentFig.src = filename;
+   }//end of loop
+   
+}//end of popularFigures()
 
 
 
@@ -41,15 +51,70 @@ function leftArrow() {
          photoOrder[i] -= 1;
       }
       populateFigures();
+      function previewFive() {
+         // create figure and img elements for fifth image
+         var lastFigure = document.createElement("figure");
+         lastFigure.id = "fig5";
+         lastFigure.style.zlndex = "5";
+         lastFigure.style.position = "absolute";
+         lastFigure.style.right = "45px";
+         lastFigure.style.top = "67px";
+         
+         var lastImage = document.createElement("img");
+         lastImage.width = "240";
+         lastImage.height = "135";
+         
+         lastfigure.appendChild(lastImage);
+         
+         articleEl.appendChild(lastFigure);
+         
+         //clone figure element for fifth image and edit to be first image
+         var firstFigure = lastFigure.cloneNode(true);
+         
+         firstFigure.id = "fig1";
+         firstFigure.style.right="";
+         firstFigure.style.left = "45px";
+         
+         articleEl.insertBefore(firstFigure, document.getElementByld)("fig2"))
+      }//end of previewFive()
    }//end of for loop
 }//end of leftArrow Function
 
-
-
+// create event listeners for left arrow, right arrow, and center figure element
+function createEventListeners() {
+   var leftarrow = document.getelementByld("leftarrow");
+   if (leftarrow.addEventListener) {
+      leftarrow.addEventListener("click",leftArrow,false);
+   } else if (leftarrow.attachEvent) {
+      leftarrow.attachEvent("onclick",leftArrow);
+      
+   var showAllButton = document.querySelector("#fiveButton p");
+   if (showAllButton.addEventListener) {
+      showAllButton.addEventListener("click",previewFive, false);
+   } else if (showAllButton.attachEvent) {
+      showAllButton.attachEvent("onclick",previewFive);
+   }//end of else if
+      
+   }//end of else if
+   }
 
 
 /* create event listeners and populate image elements */
 function setUpPage() {
+   var rightarrow = document.getElementByld("rightarrow");
+   if (rightarrow.addEventListener) {
+      rightarrow.addEventListener("click,rightArrow,false");
+   } else if (rightarrow.attachEvent) {
+      rightarrow.attachEvent("onclick",rightArrow);
+   var showAllButton = document.querySelector("#fiveButton p");
+   if (showAllButton.addEventListener) {
+      showAllButton.addEventListener("click",previewFive, false);
+   } else if (showAllButton.attachEvent) {
+      showAllButton.attachEvent("onclick",previewFive);
+   }
+   }// end of else if
+}//end of createEventListeners function   
+
    createEventListeners();
    populateFigures();
 }//end of setUpPage Function
